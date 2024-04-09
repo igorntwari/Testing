5ae141bb3dbdaef4e0cc5a8b9266834e60b40500
igorntwari <igorntwari28@gmail.com>
2024-04-09 12:10:46 +0200

    feat:added intergration tests

--- testing01/util.test.js
+++ testing01/util.test.js
@@ -1,6 +1,11 @@
-const {generateText} = require('./util')
+const {generateText,checkAndGenerate} = require('./util')
 
 test('it should output name and age',()=> {
     const text = generateText('igor',12)
     expect(text).toBe('igor (12 years old)')
+})
+
+test('it shoul return the right output',()=> {
+    const text = checkAndGenerate('ntwali',99)
+    expect(text).toBe(`ntwali (${99} years old)`)
 })
